f6b67be1211653f0b2d7b3b62a49f0a950297db1
ranjit1024 <ranjitdas2048@gmail.com>
2024-09-27 17:55:16 +0530

    making some changes

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const done = document.querySelector('.done')
 
 function createList(){
     let taskData = `<p class="task-content"><span>task : </span>${form.task.value}</p>
-                    <p class="task-discripition"><span>task descripition : </span>${form.descripition.value}</p>`
+                    <p class="task-discripition"><span>task descripition : </span>${form.descripition.value}</p>
+                    <button class="done">Mark as Done</button>`
     return taskData;
 }
 
@@ -15,13 +16,13 @@ form.addEventListener("submit", (e)=>{
     taskConent.classList.add('task-container')
     taskConent.innerHTML = createList(); 
     task.appendChild(taskConent);
+    const done = document.querySelector(".done");
+    
+    done.addEventListener("click", (e)=>{
+        const parentElement = e.target.parentElement;
+        parentElement.remove()
+    })
     form.reset()
     
 })
 
-done.addEventListener("click", (e)=>{
-    const taskContainer = document.querySelector(".task-container");
-    taskContainer.remove();
-    
-
-})
